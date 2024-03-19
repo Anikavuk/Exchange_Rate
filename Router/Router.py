ab929b6e75fd287268a4d7f0d352c9ffaa1e94e3
Anikavuk <Usla_2018@mail.ru>
2024-03-19 16:44:39 +0500

    Changed the Router folder

--- Router/Router.py
+++ Router/Router.py
@@ -4,29 +4,8 @@ from controller.rate_controller import RateController
 from controller.rates_controller import RatesController
 from service.service import ServiceExchange
 
-routes = {
-
-    '/currencies':
-        {'method':
-             {'get': CurrenciesController.do_GET(),
-              'post': CurrenciesController.do_POST()},
-         },
-    '/currency':
-        {'method':
-             {'get': CurrencyController.do_GET()}
-         },
-    '/exchangeRates':
-        {'method':
-             {'get': RatesController.do_GET(),
-              'post': RatesController.do_POST()}
-         },
-    '/exchangeRate':
-        {'method':
-             {'get': RateController.do_GET(),
-              'patch': RateController.do_PATCH()}
-         },
-    '/exchange':
-        {'method':
-             {'get': ServiceExchange.do_GET()}
-         }
-}
+routes = {'/currencies': CurrenciesController,
+          '/currency': CurrencyController,
+          '/exchangeRates': RatesController,
+          '/exchangeRate': RateController,
+          '/exchange': ServiceExchange}
