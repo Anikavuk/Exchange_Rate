fb06862066f7085669c790a8df131d9da3304123
Anikavuk <Usla_2018@mail.ru>
2024-04-11 20:15:36 +0500

    Исправлен путь к базе данных

--- env.py
+++ env.py
@@ -1,6 +1,7 @@
 """Переменная для обозначения пути базы данных"""
-path_to_database = "C:\\Users\\HP\\PycharmProjects\\Exchange_Rate\\Data_storage.db"
+# path_to_database = "C:\\Users\\HP\\PycharmProjects\\Exchange_Rate\\Data_storage.db"
 # path_to_database = "Currencies_Exchange\\Data_storage.db" # для проверки ошибки "База данных недоступна"
-# path_to_database = '/project_folder/Exchange_Rate/Data_storage.db'
+#path_to_database = '/project_folder/Exchange_Rate/Data_storage.db'
+path_to_database = '/root/project_folder/Exchange_Rate/Data_storage.db'
 
 # path_to_database = "C:\Users\HP\PycharmProjects\Exchange_Rate\Data_storage.db"
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import http.server
 from server import Server
 
-server = http.server.HTTPServer(('localhost', 8080), Server)
+server = http.server.HTTPServer(('0.0.0.0', 8080), Server)
 
 if __name__ == '__main__':
     server.serve_forever()
