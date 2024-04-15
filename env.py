f32b65221be1d5cf631b6c93fc31ece2ec8c976f
Anikavuk <usla_2018@mail.ru>
2024-04-15 21:10:38 +0500

    Исправлен env.py

--- env.py
+++ env.py
@@ -1,8 +1,6 @@
 """Переменная для обозначения пути базы данных"""
-# path_to_database = "C:\\Users\\HP\\PycharmProjects\\Exchange_Rate\\Data_storage.db"
-path_to_database = 'C:\\Users\HP\\PycharmProjects\\pythonProject5\\Data_storage.db'
+path_to_database = "C:\\Users\\HP\\PycharmProjects\\Exchange_Rate\\Data_storage.db"
 # path_to_database = "Currencies_Exchange\\Data_storage.db" # для проверки ошибки "База данных недоступна"
-#path_to_database = '/project_folder/Exchange_Rate/Data_storage.db'
-# path_to_database = '/root/project_folder/Exchange_Rate/Data_storage.db'
 
-# path_to_database = "C:\Users\HP\PycharmProjects\Exchange_Rate\Data_storage.db"
+# path_to_database = '/root/project_folder/Exchange_Rate/Data_storage.db' путь базы данных на сервере
+
