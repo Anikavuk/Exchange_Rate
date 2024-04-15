9c242c833ba7e88c7251b76675148c54778c6e3a
Anikavuk <usla_2018@mail.ru>
2024-04-15 19:27:49 +0500

    проверка git1

--- proverka_git.py
+++ proverka_git.py
@@ -1,1 +1,4 @@
 'Solution: Settings => Version Control => Git => ticked "Use credential helper" and then it worked perfect.'
+
+'Solution: Settings => Version Control => Git => ticked "Use credential helper" and then it worked perfect.'
+'Solution: Settings => Version Control => Git => ticked "Use credential helper" and then it worked perfect.'
