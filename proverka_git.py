3fc9223a1838ec86851c00cbfecc431628e39e83
Anikavuk <usla_2018@mail.ru>
2024-04-15 19:46:40 +0500

    Delete files

--- proverka_git.py
+++
@@ -1,4 +0,0 @@
-'Solution: Settings => Version Control => Git => ticked "Use credential helper" and then it worked perfect.'
-
-'Solution: Settings => Version Control => Git => ticked "Use credential helper" and then it worked perfect.'
-'Solution: Settings => Version Control => Git => ticked "Use credential helper" and then it worked perfect.'
